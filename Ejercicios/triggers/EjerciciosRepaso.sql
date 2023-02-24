b398c67789e1565d1c327e34a4d883ea9d815ec6
bernat <bernat13@gmail.com>
2023-02-24 09:55:17 +0100

    tareas repaso

---
+++ Ejercicios/triggers/EjerciciosRepaso.sql
@@ -0,0 +1,32 @@
+--En el arepezo
+
+
+--1.-Cuando un cliente añada un pedido, mirar si es el cliente que más se ha gastado, si es así, añadir unos tequeños a precio 0 a su pedido
+
+--2.-Cuando un cliente añada un pedido, mirar si es el cliente con más pedidos ese mes, si es así, invitarle a yuca frita.
+
+
+--3.-Promoción en el arepezo, si compras uja arepa viuda, todos los elementos que tuviera tu pedido en pedidoslinea, reciben un descuento del 5% sobre el precio unitario.
+
+
+--Use laliga (con tabla estadísticas)
+
+--4.-Cuando un jugador reciba una tarjeta roja, quitarle un 1% de su sueldoanual.
+
+
+--5.-Cuando un jugador meta 3 golea en un mismo partido, sumarle un 1% en su sueldo anual
+
+--6.-Cuando un jugador meta un gol (tabla estadísticas), sumar un gol a su equipo en el marcador (tabla partidos)
+
+--7.-Cuando en la tabla clasificación algún equipo gane 38 partidos, duplicar el sueldo de todos  suas jugadores.
+
+
+--BBDD Twitter.
+
+--8.-Añadir una tabla facturación con usuario, nick y dineroganado.
+
+--9.-Cuando un usuario reciba un like en uno de sus tweets, debe incrementarse en 2 céntimos su dinero ganado en la nueva tabla facturación. Si el usuario no está en la tabla facturación, deberemos añadirlo.
+
+--10.-Si un usuario pública un tuit con la palabra caca, borrar borrar cambiar el nombre del usuario por 'cuentabloqueada'
+
+--11.-Haced una tabla histórico y el trigger correspondiente como hemos visto en clase de la tabla usuarios
